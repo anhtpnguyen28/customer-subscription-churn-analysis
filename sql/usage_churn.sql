86cafa4ee785775e70dbcfcba36d4bebf6f82676
Anh Nguyen <anhtpnguyen28@gmail.com>
2026-02-09 23:13:00 -0500

    Add usage vs churn SQL skeleton

---
+++ sql/usage_churn.sql
@@ -0,0 +1,9 @@
+-- Business Question:
+-- How does user engagement (usage) relate to churn?
+
+-- Churn Definition:
+-- A user is considered churned if churn = 'Yes'
+
+-- Metrics:
+-- Average weekly usage by churn status
+-- Churn rate by usage level
