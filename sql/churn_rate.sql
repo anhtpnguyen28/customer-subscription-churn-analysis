a8fbea4195af7e508b6cd5b56167f332169cb801
Anh Nguyen <anhtpnguyen28@gmail.com>
2026-02-09 22:52:11 -0500

    Add churn rate SQL skeleton

---
+++ sql/churn_rate.sql
@@ -0,0 +1,8 @@
+-- Business Question:
+-- What is the monthly churn rate?
+
+-- Churn Definition:
+-- A customer is considered churned if churn_status = 'Yes'
+
+-- Metrics:
+-- Monthly churn rate = churned customers / total customers
