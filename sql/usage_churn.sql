b3cc6a2dfb7a04f9ee5183ec63d5dba02aa418d0
Anh Nguyen <anhtpnguyen28@gmail.com>
2026-02-09 23:15:38 -0500

    Update usage_churn.sql

--- sql/usage_churn.sql
+++ sql/usage_churn.sql
@@ -7,3 +7,42 @@
 -- Metrics:
 -- Average weekly usage by churn status
 -- Churn rate by usage level
+
+
+-- Analysis 1:
+-- Compare average usage between churned and retained users
+
+SELECT
+    churn,
+    COUNT(DISTINCT user_id) AS total_users,
+    AVG(avg_weekly_usage_hours) AS avg_weekly_usage
+FROM customer_subscriptions
+GROUP BY churn;
+
+-- --------------------------------------------
+
+-- Analysis 2:
+-- Analyze churn rate by usage level
+
+WITH usage_bucket AS (
+    SELECT
+        user_id,
+        churn,
+        CASE
+            WHEN avg_weekly_usage_hours < 5 THEN 'Low Usage'
+            WHEN avg_weekly_usage_hours BETWEEN 5 AND 10 THEN 'Medium Usage'
+            ELSE 'High Usage'
+        END AS usage_level
+    FROM customer_subscriptions
+)
+
+SELECT
+    usage_level,
+    COUNT(DISTINCT user_id) AS total_users,
+    COUNT(DISTINCT CASE WHEN churn = 'Yes' THEN user_id END) AS churned_users,
+    COUNT(DISTINCT CASE WHEN churn = 'Yes' THEN user_id END) * 1.0
+        / COUNT(DISTINCT user_id) AS churn_rate
+FROM usage_bucket
+GROUP BY usage_level
+ORDER BY churn_rate DESC;
+
