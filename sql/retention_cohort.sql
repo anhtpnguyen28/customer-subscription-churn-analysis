3a4da1f3675f77c1e3e4202d4c87d126a55678f1
Anh Nguyen <anhtpnguyen28@gmail.com>
2026-02-09 23:01:12 -0500

    Add retention cohort SQL skeleton

---
+++ sql/retention_cohort.sql
@@ -0,0 +1,11 @@
+-- Business Question:
+-- How does customer retention change over time by signup cohort?
+
+-- Churn Definition:
+-- A customer is considered churned if churn_status = 'Yes'
+
+-- Cohort Definition:
+-- Cohort is defined by customer signup month
+
+-- Metrics:
+-- Retention rate by cohort and month
